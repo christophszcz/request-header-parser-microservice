aa62cf80f1c1a902d2ffb9f52fc6d10fb1f04b1c
Chris Szczechowicz <info@cjszczechowicz.com>
2022-06-24 02:33:16 -0400

    Update the favicon

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-// app.get("/api/hello", function (req, res) {
-//   res.json({greeting: 'hello API'});
-// });
+app.get("/api/whoami", function (req, res) {
+  // res.json({ ipaddress: });
+});
 
 // listen for requests
 var listener = app.listen(process.env.PORT || 3000, function () {
