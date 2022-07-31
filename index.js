15c1ef0a904d521d5c25b6627754f89b63633a76
Chris Szczechowicz <info@cjszczechowicz.com>
2022-07-30 21:38:21 -0400

    Get the user\'s software information

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/whoami", function (req, res) {
+  const userInfo = { language: undefined, language: undefined, software: undefined };
   const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const language = req.headers['accept-language'];
-  res.json({ ipaddress: ipAddress, language: language });
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress: ipAddress, language: language, software: software });
 });
 
 // listen for requests
